d2c6f17a4f6e3a0a6e4eef66a45625b1c92b85ba
Ahmed Shahzad Kayani <ahmedkayani786@yahoo.com>
2023-09-13 20:43:55 +0500

    Line Chart Removed due to errors

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -123,7 +123,6 @@ export function Home() {
           <Doughnut data={data} options={options} />
         </Card>
         {/* This is the line graph chart */}
-        <LineChart />
       </div>
 
       <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
